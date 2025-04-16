4843edc3ce59abb91e50413c80935c6ea8b8878d
Sharan kumar <bssharankumar28920@gmail.com>
2025-04-16 14:14:49 +0530

    reverse done

--- reverse_array.py
+++ reverse_array.py
@@ -13,3 +13,14 @@
 
 # a=reverse_array([1,2,3,4,5])
 # print(a)
+
+
+# second type 
+# def reverse(arr):
+#     l,r=0,len(arr)-1
+#     while l<r:
+#         arr[l],arr[r]=arr[r],arr[l]
+#         l,r=l+1,r-1
+#     return arr
+# a=reverse([1,2,3])
+# print(a)
