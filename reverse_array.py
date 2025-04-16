1c3dc633b2ae6b84fa55450adbab0259f5e82364
Sharan kumar <bssharankumar28920@gmail.com>
2025-04-16 14:04:20 +0530

    2 methods added

---
+++ reverse_array.py
@@ -0,0 +1,15 @@
+
+# using built in function
+
+# list=[1,2,3,4,5]
+# print("before reversing: ",list)
+# list.reverse()
+# print("after reversing: ",list)
+
+
+# using user-biult-in function
+# def reverse_array(arr):
+#     return arr[::-1]
+
+# a=reverse_array([1,2,3,4,5])
+# print(a)
