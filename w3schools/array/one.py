0dec61a5661ad787bb8601bec0588de082c53c9e
Sharan kumar <bssharankumar28920@gmail.com>
2025-08-06 09:41:05 +0530

    practicing w3 schools array

---
+++ w3schools/array/one.py
@@ -0,0 +1,8 @@
+arr=[1,2,3,4,16,25,44,0,45]
+minVal=arr[0]
+for i in arr:
+    if i<minVal:
+        minVal=i
+print(f"The minimum value in the array is:",minVal) 
+        
+
---
+++ w3schools/introduction/fib.py
@@ -0,0 +1,11 @@
+# using for loop 
+
+num1=0
+num2=1
+print(num1)
+print(num2)
+for fib in range(100):
+    num3=num1+num2
+    print(num3)
+    num1=num2
+    num2=num3
